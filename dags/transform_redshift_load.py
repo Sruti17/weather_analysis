9e2c38e96ed8fa8c799922776ba6c54f340fe087
Sruti shriya sahu <2002sruti@gmail.com>
2024-06-21 12:43:49 +0530

    chg

--- dags/transform_redshift_load.py
+++ dags/transform_redshift_load.py
@@ -13,14 +13,19 @@ default_args={
 dag =DAG('transform_redshift_dag', default_args=default_args, schedule_interval="@once", catchup=False)
 
 #Define the Glue Job
-transform_task =GlueJobOperator(
-    task_id ='transform_task',
-    job_name ="glue_transform_task",
-    script_location ='s3://aws-glue-assets-637423317099-ap-southeast-1/scripts/weather_data_ingestion.py',
-    s3_bucket='s3://aws-glue-assets-637423317099-ap-southeast-1/scripts/',
-    aws_conn_id ='aws_default',
-    region_name ="ap-southeast-1",
-    iam_role_name ='my-role-for-proj',
-    create_job_kwargs ={"GlueVersion":"4.0", "NumberOfWorkers":2, "WorkerType": "G.1X","Connections":{"Connections":["Redshift connection"]},},
+transform_task = GlueJobOperator(
+    task_id='transform_task',
+    job_name='glue_transform_task',
+    script_location='s3://aws-glue-assets-637423317099-ap-southeast-1/scripts/weather_data_ingestion.py',
+    s3_bucket='aws-glue-assets-637423317099-ap-southeast-1',  # s3_bucket should not include 'scripts/'
+    aws_conn_id='aws_default',
+    region_name='ap-southeast-1',
+    iam_role_name='my-role-for-proj',
+    create_job_kwargs={
+        'GlueVersion': '4.0',
+        'NumberOfWorkers': 2,
+        'WorkerType': 'G.1X',
+        'Connections': {'Connections': ['Redshift connection']}
+    },
     dag=dag,
 )
